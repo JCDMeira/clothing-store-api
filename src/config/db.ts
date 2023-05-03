beed75055b85a8b833c50158219e57c0a1d74718
Jean Meira <jean.meira10@hotmail.com>
2023-05-02 21:02:05 -0300

    chore: add mongose

---
+++ src/config/db.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.set('strictQuery', true);
+
+const mongoUrl = process.env.CONNECTION_STRING as string;
+
+mongoose.connect(mongoUrl);
+
+const db = mongoose.connection;
+
+db.on('error', console.log.bind(console, 'Connection error'));
+db.once('opne', () => console.log('sucessfull connection'));
+
+export { db };

a56e4d04b1cf8646e60fc60d6e576a26d72d3c9f
Jean Meira <jean.meira10@hotmail.com>
2023-05-11 21:59:21 -0300

    refactor: product model

--- src/models/Product.model.ts
+++ src/models/Product.model.ts
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 const ProductSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: false },
-  modifiers: { type: Array<string>, required: false },
-  sets: { type: Array<string>, required: false },
+  modifiers: { type: Array<{ title: string; price: number }>, required: false },
+  sets: { type: Array<{ title: string; price: number }>, required: false },
   price: { type: Number, required: true },
   created_at: { type: Number, required: true },
   updated_at: { type: Number, required: true },

084f657d607696dea5200c2f1242ad7841465a0f
Jean Meira <jean.meira10@hotmail.com>
2023-05-15 21:11:57 -0300

    feat: add photo on product

--- src/models/Product.model.ts
+++ src/models/Product.model.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const ProductSchema = new mongoose.Schema({
   title: { type: String, required: true },
+  photo: { type: String, required: true },
   description: { type: String, required: false },
   modifiers: { type: Array<{ title: string; price: number }>, required: false },
   sets: { type: Array<{ title: string; price: number }>, required: false },
